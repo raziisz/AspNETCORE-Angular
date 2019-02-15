66a836dc417dc21439538a4368126bb9a28505b2
Luiz Felipe <raziel_libertino@hotmail.com>
2019-02-15 16:42:37 -0400

    Associando dblite e criando a primeira a classe

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -1,7 +1,15 @@
+using DatingApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
 namespace DatingApp.API.Data
 {
-    public class DataContext
+    public class DataContext : DbContext
     {
-        
+        public DataContext(DbContextOptions<DataContext> options) : 
+        base(options)
+        {            
+        }
+
+        public DbSet<Value> Values { get; set; }
     }
 }
